f0adc3a178fe7f9f31732cafb749cad975cbdcb7
Mestre Santiago <souto.santiago@academico.ifpb.edu.br>
2024-05-04 18:35:57 -0300

    Atualização no header e no banner

--- src/components/banner/index.tsx
+++ src/components/banner/index.tsx
@@ -1,11 +1,11 @@
 import { ContactButton } from "../contactButton/Index";
-import avatar from "../../assets/avatar.webp";
+import avatar from "../../assets/avatar.png";
 export function Banner() {
   return (
-    <section id="inicio" className="bg-pink-300 text-white w-screen flex justify-center items-center py-10 pt-28">
-      <div className="max-w-[1024px] px-6 w-screen flex flex-col items-center justify-center gap-16 lg:flex-row">
+    <section id="inicio" className="bg-cover bg-bottom text-white w-screen flex flex-col justify-end items-center pt-32 lg:pt-16">
+      <div className="max-w-[1024px] px-6 w-screen flex flex-col items-center justify-end gap-0 lg:gap-16 lg:flex-row">
         <div className="flex-1 lg:w-[50%]">
-          <h1 className="text-4xl font-bold mb-7 lg:text-5xl">Vamos a luta por uma Remígio melhor</h1>
+          <h1 className="text-3xl font-bold mb-7 lg:text-5xl">Vamos à luta com a força da nossa terra</h1>
           <p className="text-base mb-7">
             Vamos juntos fazer de Remígio um lugar de progresso e felicidade
             para nossas famílias. A jornada é desafiadora, mas com fé, união e
@@ -14,6 +14,9 @@ export function Banner() {
           <ContactButton color="#0D68AF" />
         </div>
         <img src={avatar} alt="" className=" rounded-lg flex-1 lg:w-[50%]"/>
+      </div>
+      <div id="padrao" className="lg:bg-contain bg-cover h-20 lg:h-32 w-full d-flex ">
+        
       </div>
     </section>
   );
--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,89 +1,12 @@
-import { ContactButton } from "../contactButton/Index";
+import img from "../../assets/logo.png"
 import { Menu } from "@headlessui/react";
 
 export default function Header() {
   return (
     <header className="w-full flex items-center justify-center bg-blue-300 px-6 py-4 fixed z-50">
       <div className="max-w-[1024px] w-full flex items-center justify-between lg:flex-row-reverse">
-        <ContactButton color="#E23C54" />
-        <div className="lg:hidden">
-        <Menu>
-          <Menu.Button className="text-base text-white">Menu</Menu.Button>
-          <Menu.Items className="bg-blue-300 text-white fixed w-screen top-14 left-0 transition-all border-none px-8 py-4 flex flex-col items-end gap-4">
-            <Menu.Item>
-              {({ active }) => (
-                <a
-                  className={`${active && "bg-blue-500"}`}
-                  href="#inicio"
-                >
-                  Inicio
-                </a>
-              )}
-            </Menu.Item>
-            <Menu.Item>
-              {({ active }) => (
-                <a
-                  className={`${active && "bg-blue-500"}`}
-                  href="#sobre"
-                >
-                  Sobre
-                </a>
-              )}
-            </Menu.Item>
-            <Menu.Item>
-              {({ active }) => (
-                <a
-                  className={`${active && "bg-blue-500"}`}
-                  href="#propostas"
-                >
-                  Propostas
-                </a>
-              )}
-            </Menu.Item>
-            <Menu.Item>
-              {({ active }) => (
-                <a
-                  className={`${active && "bg-blue-500"}`}
-                  href="#galeria"
-                >
-                  Galeria
-                </a>
-              )}
-            </Menu.Item>
-            <Menu.Item>
-              {({ active }) => (
-                <a
-                  className={`${active && "bg-blue-500"}`}
-                  href="#depoimentos"
-                >
-                  Depoimentos
-                </a>
-              )}
-            </Menu.Item>
-            <Menu.Item>
-              {({ active }) => (
-                <a
-                  className={`${active && "bg-blue-500"}`}
-                  href="#dicionario"
-                >
-                  Dicionário
-                </a>
-              )}
-            </Menu.Item>
-            <Menu.Item>
-              {({ active }) => (
-                <a
-                  className={`${active && "bg-blue-500"}`}
-                  href="#contato"
-                >
-                  Redes Sociais
-                </a>
-              )}
-            </Menu.Item>
-          </Menu.Items>
-        </Menu>
 
-        </div>
+
         <nav className="hidden lg:flex items-center gap-10 text-white">
           <a href="#inicio">
             Inicio
@@ -107,7 +30,85 @@ export default function Header() {
             Redes Sociais
           </a>
         </nav>
+        <img src={img} alt="Logo de Gleds" className="w-32 z-50" />
+        <div className="lg:hidden">
+          <Menu>
+            <Menu.Button className="text-base text-white">Menu</Menu.Button>
+            <Menu.Items className="bg-blue-300 text-white fixed w-screen top-14 left-0 transition-all border-none px-8 py-4 flex flex-col items-end gap-4">
+              <Menu.Item>
+                {({ active }) => (
+                  <a
+                    className={`${active && "bg-blue-500"}`}
+                    href="#inicio"
+                  >
+                    Inicio
+                  </a>
+                )}
+              </Menu.Item>
+              <Menu.Item>
+                {({ active }) => (
+                  <a
+                    className={`${active && "bg-blue-500"}`}
+                    href="#sobre"
+                  >
+                    Sobre
+                  </a>
+                )}
+              </Menu.Item>
+              <Menu.Item>
+                {({ active }) => (
+                  <a
+                    className={`${active && "bg-blue-500"}`}
+                    href="#propostas"
+                  >
+                    Propostas
+                  </a>
+                )}
+              </Menu.Item>
+              <Menu.Item>
+                {({ active }) => (
+                  <a
+                    className={`${active && "bg-blue-500"}`}
+                    href="#galeria"
+                  >
+                    Galeria
+                  </a>
+                )}
+              </Menu.Item>
+              <Menu.Item>
+                {({ active }) => (
+                  <a
+                    className={`${active && "bg-blue-500"}`}
+                    href="#depoimentos"
+                  >
+                    Depoimentos
+                  </a>
+                )}
+              </Menu.Item>
+              <Menu.Item>
+                {({ active }) => (
+                  <a
+                    className={`${active && "bg-blue-500"}`}
+                    href="#dicionario"
+                  >
+                    Dicionário
+                  </a>
+                )}
+              </Menu.Item>
+              <Menu.Item>
+                {({ active }) => (
+                  <a
+                    className={`${active && "bg-blue-500"}`}
+                    href="#contato"
+                  >
+                    Redes Sociais
+                  </a>
+                )}
+              </Menu.Item>
+            </Menu.Items>
+          </Menu>
 
+        </div>
       </div>
     </header>
   );
