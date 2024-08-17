de1b645713c2d9cd32d9a7a4a35650ba9c2dc8ad
Santiago <souto.santiago@academico.ifpb.edu.br>
2024-08-17 04:25:37 -0300

    mudança para nextjs

--- src/pages/home/components/contactButton/Index.tsx
+++
@@ -1,9 +0,0 @@
-
-
-export function ContactButton(){
-  return(
-    <a href="/remigio-da-gente" target="_blank" className="text-base rounded-lg text-white py-2 px-5 bg-purple-300">
-      Remígio da gente
-    </a>
-  )
-}
--- src/types/planoDeGoverno.tsx
+++
@@ -1,6 +0,0 @@
-export type ThematicArea = {
-  title: string;
-  specificObjective: string;
-  proposals: string[];
-  icon: React.ReactNode; // Agora aceitando um ReactNode
-};
